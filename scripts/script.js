1571a41eb6d5be60a7f563e1c5c81f2d4bce627c
Alina Miao <amiao00@berkeley.edu>
2021-07-13 14:27:43 -0700

    created interface, gameplay logic for every round of rps

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,75 @@
-let gameOptions = ["rock", "paper", "scissors"]
 
 function computerPlay() {
+	let gameOptions = ["rock", "paper", "scissors"];
   return gameOptions[Math.floor(Math.random() * gameOptions.length)];
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
+	let roundOutcome;
   if (playerSelection === computerSelection) {
-    return "You tie! Play another round!";
+    roundOutcome = "We chose the same weapon. We tied! Play another round.";
   } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
       (playerSelection === "paper" && computerSelection === "rock") || 
       (playerSelection === "scissors" && computerSelection === "paper")) {
-    return `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}!`;
+    roundOutcome = `You chose ${capitalize(playerSelection)}. I chose ${capitalize(computerSelection)}.
+				You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}.
+				The score is ${playerScore} - ${computerScore}.`;
+		playerScore += 1;
   } else {
-    return `You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`;
+    roundOutcome = `You chose ${capitalize(playerSelection)}. I chose ${capitalize(computerSelection)}.
+				You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}. 
+				The score is ${playerScore} - ${computerScore}.`;
+		computerScore += 1;
   }
+
+	//change/display the scores and message here 
+	const container = document.querySelector("main");
+
+	let scoreMessage = document.createElement("div");
+	scoreMessage.id = "score-message";
+	scoreMessage.textContent = roundOutcome;
+	container.appendChild(scoreMessage);
+
+	/*if (container.contains(scoreMessage)) {
+		//container.removeChild(scoreMessage);
+		container.appendChild(scoreMessage);
+	} else {
+		container.appendChild(scoreMessage);
+	} */
+
+	if (playerScore === 5){
+		console.log('You won!')
+		//set up div displaying final scores and restart game button
+	} else if (computerScore === 5) {
+		console.log('You lost!')
+		//set up div displaying final scores and restart game button
+	}	else {
+		return;
+	//create the div that contains the text we want to display per round
+	}
 }
 
-/*helper function to capitalize the first letter of lowercase strings*/
+const rpsButtons = document.querySelectorAll(".rps-button");
+rpsButtons.forEach((button) => {
+  button.addEventListener("click", function(evt) {
+    playRound(evt.target.id, computerPlay());
+  });
+});
+
+//helper function to capitalize the first letter of lowercase strings
 function capitalize(s) {
   if (typeof s !== 'string')
     return '';
   return s.charAt(0).toUpperCase() + s.slice(1);
 }
 
+
+//Plays a 5 rounds of rock paper scissors
+/*
 function game() {
   let playerScore = 0;
   let computerScore = 0;
@@ -49,3 +95,4 @@ function game() {
 }
 
 console.log(game())
+*/
